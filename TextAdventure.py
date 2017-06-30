64cf225ed4b7618ae7cb5728cbc2020e0c365b3c
JaniceL <janicel@Macs-MacBook-Pro-99.local>
2017-06-30 11:31:23 -0700

    updated code

--- TextAdventure.py
+++ TextAdventure.py
@@ -15,3 +15,22 @@ if FoodChoice == 1:
             print("Your stomach makes noises and you barf on the person ahead of you! :(\n")
     if StomachPain == 2:
         print("You fart and it stinks up the whole room. Your friend walks away and you are left alone.\n")
+
+print("You go to a restaurant that primarily serves spicy food, but you can't digest spicy food well.Do you order a cheesy burrito or an expensive salad")
+FoodChoice == 0
+FoodChoice == int(input("Which option do you prefer?\n"))
+
+if FoodChoice == 2:
+    print("You received your salad, but you see that it tastes very plain, so you don't feel like eating it. You want to call over a waiter, but the staff is super busy. You want sauce or additional food. Do you continue to eat the plain salad or wait for a waiter to serve you? \n")
+
+    print("Press 3 for the first option and 4 for the second.\n")
+if FoodChoice == 3:
+    print("After eating most of the salad, you still feel hungry. You go home hungry. :( )\n")
+
+if FoodChoice == 4:
+    print("After waiting for 10 minutes, the waiter offers to give you nachos and beans. But you must wait for another 20 minutes.\n")
+    print("Do you wait for another 20 minutes or do you go home hungry?\n")
+if FoodChoice == 5:
+    print("You go home happy! :)\n")
+if FoodChoice == 6:
+        print("Too bad. You go home hungry :().\n")
